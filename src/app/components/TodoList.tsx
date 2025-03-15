ec5497d936d6ae351bccede21f5d7d1a9402fd6d
dev-rjw <rjw0105@gmail.com>
2025-03-16 00:41:23 +0900

    5. 투두 구현 - Create 추가

--- src/app/components/TodoList.tsx
+++ src/app/components/TodoList.tsx
@@ -1,10 +1,6 @@
-import { useQuery } from "@tanstack/react-query";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import axios from "axios";
-
-const getTodos = async () => {
-  const result = await axios.get("http://localhost:3000/todos");
-  return result.data;
-};
+import { useState } from "react";
 
 type Result = {
   id: string;
@@ -12,10 +8,30 @@ type Result = {
   completed: boolean;
 };
 
+const getTodos = async () => {
+  const result = await axios.get("http://localhost:3000/todos");
+  return result.data;
+};
+
+const setTodo = async (addTodo: Result) => {
+  const result = await axios.post("http://localhost:3000/todos", addTodo);
+  return result;
+};
+
 const TodoList = () => {
+  const [title, setTitle] = useState<string>("");
+  const queryClient = useQueryClient();
+
   const { data, isLoading, isError } = useQuery({
     queryKey: ["todos"],
     queryFn: getTodos,
+  });
+
+  const mutation = useMutation({
+    mutationFn: setTodo,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["todos"] });
+    },
   });
 
   if (isLoading) {
@@ -26,8 +42,19 @@ const TodoList = () => {
     return <div>에러가 발생했습니다.</div>;
   }
 
+  const addTodo = async () => {
+    mutation.mutate({
+      id: new Date().getTime().toString(),
+      title: title,
+      completed: false,
+    });
+    setTitle("");
+  };
+
   return (
     <div>
+      <input type="text" value={title} name="title" onChange={(event) => setTitle(event.target.value)} />
+      <button onClick={addTodo}>일정 추가하기</button>
       {data?.map((result: Result) => {
         return (
           <div key={result.id}>
