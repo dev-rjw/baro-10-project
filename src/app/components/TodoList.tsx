4ed5292a6b883c2c0b4703df92b60f7b47a7ab33
dev-rjw <rjw0105@gmail.com>
2025-03-16 15:40:35 +0900

    5. 투두 구현 - Update 추가(title, completed) + 매개변수 이름 변경 + 주석 추가

--- src/app/components/TodoList.tsx
+++ src/app/components/TodoList.tsx
@@ -2,36 +2,52 @@ import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import axios from "axios";
 import { useState } from "react";
 
-type Result = {
+// Todo 타입 선언
+type Todo = {
   id: string;
   title: string;
   completed: boolean;
 };
 
+// axios로 todos 가져오기
 const getTodos = async () => {
   const result = await axios.get("http://localhost:3000/todos");
   return result.data;
 };
 
-const addTodo = async (addTodo: Result) => {
-  const result = await axios.post("http://localhost:3000/todos", addTodo);
+// axios로 todo 추가하기
+const addTodo = async (todo: Todo) => {
+  const result = await axios.post("http://localhost:3000/todos", todo);
   return result;
 };
 
+// axios로 todo 삭제하기
 const deleteTodo = async (id: string) => {
   const result = await axios.delete(`http://localhost:3000/todos/${id}`);
   return result;
 };
 
+// axios로 todo 변경하기
+const updateTodo = async (todo: Todo) => {
+  const result = await axios.patch(`http://localhost:3000/todos/${todo.id}`, todo);
+  return result;
+};
+
+// 메인 함수
 const TodoList = () => {
   const [title, setTitle] = useState<string>("");
+  const [editId, setEditId] = useState<string | null>(null);
+  const [editTitle, setEditTitle] = useState<string>("");
+
   const queryClient = useQueryClient();
 
+  // useQuery 사용 - getTodos
   const { data, isLoading, isError } = useQuery({
     queryKey: ["todos"],
     queryFn: getTodos,
   });
 
+  // useMutation 사용 - addTodo
   const addMutation = useMutation({
     mutationFn: addTodo,
     onSuccess: () => {
@@ -39,8 +55,17 @@ const TodoList = () => {
     },
   });
 
+  // useMutation 사용 - deleteTodo
   const deleteMutation = useMutation({
     mutationFn: deleteTodo,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["todos"] });
+    },
+  });
+
+  // useMutation 사용 - updateTodo
+  const updateMutation = useMutation({
+    mutationFn: updateTodo,
     onSuccess: () => {
       queryClient.invalidateQueries({ queryKey: ["todos"] });
     },
@@ -54,6 +79,7 @@ const TodoList = () => {
     return <div>에러가 발생했습니다.</div>;
   }
 
+  // Todo 추가 핸들러
   const handleAddTodo = async () => {
     addMutation.mutate({
       id: new Date().getTime().toString(),
@@ -63,25 +89,66 @@ const TodoList = () => {
     setTitle("");
   };
 
+  // Todo 삭제 핸들러
   const handleDeleteTodo = async (id: string) => {
     deleteMutation.mutate(id);
   };
 
+  // Todo Completed 변경 핸들러
+  const handleCompletedUpdateTodo = async (todo: Todo) => {
+    const updatedTodo = {
+      ...todo,
+      completed: !todo.completed,
+    };
+    updateMutation.mutate(updatedTodo);
+  };
+
+  // Todo Title 변경 시작 핸들러
+  const handleTitleUpdateTodoStart = (todo: Todo) => {
+    setEditId(todo.id);
+    setEditTitle(todo.title);
+  };
+
+  // Todo Title 변경 핸들러
+  const handleTitleUpdateTodo = async (todo: Todo) => {
+    if (editTitle.trim() !== "") {
+      const updatedTodo = {
+        ...todo,
+        title: editTitle,
+      };
+      updateMutation.mutate(updatedTodo);
+      setEditId(null);
+    } else {
+      alert("타이틀을 입력해주세요.");
+    }
+  };
+
   return (
-    <div>
+    <>
       <input type="text" value={title} name="title" onChange={(event) => setTitle(event.target.value)} />
       <button onClick={handleAddTodo}>일정 추가하기</button>
-      {data?.map((result: Result) => {
+      {data?.map((result: Todo) => {
         return (
           <div key={result.id}>
-            <div>{result.title}</div>
-            <button>{result.completed ? "취소하기" : "완료하기"}</button>
+            <button onClick={() => handleCompletedUpdateTodo(result)}>{result.completed ? "❌" : "✅"}</button>
+            {editId === result.id ? (
+              <>
+                <input type="text" value={editTitle} name="editTitle" onChange={(event) => setEditTitle(event.target.value)} />
+                <br></br>
+                <button onClick={() => handleTitleUpdateTodo(result)}>수정 완료하기</button>
+              </>
+            ) : (
+              <>
+                <div>{result.title}</div>
+                <button onClick={() => handleTitleUpdateTodoStart(result)}>수정하기</button>
+              </>
+            )}
             <button onClick={() => handleDeleteTodo(result.id)}>삭제하기</button>
             <hr></hr>
           </div>
         );
       })}
-    </div>
+    </>
   );
 };
 
